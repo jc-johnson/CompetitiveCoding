f6b54117225ec85c074efb3a0510937d6500e593
Jordan C. Johnson <jc-johnson@users.noreply.github.com>
2021-02-18 20:38:54 -0600

    Update LongestSubstringwithAtMostKCharacters.py

--- LongestSubstringwithAtMostKCharacters.py
+++ LongestSubstringwithAtMostKCharacters.py
@@ -42,10 +42,9 @@ class Solution:
         '''
         # Initializes the table for mapping characters to occurences
         map = {}
-        # left and right are pointers to navigate the given string 
-        # setting max lower bound at 0 for max() comparison later
+        # left and right are pointers to navigate the given string - we can create an inner substring to analyze using 2 pointers 
         # count the number of distinct characters we've seen
-        # count the max string length
+        # count the max string length - setting max lower bound at 0 for max() comparison later
         left, right, num_distinct_chars, maximum = 0, 0, 0, 0
         
         # Simple iteration through a string - We will utilize a left and right pointer to traverse our string
@@ -60,21 +59,27 @@ class Solution:
                 map[string[right]] = 1
                 num_distinct_chars += 1 # Increment the total number of distinct characters seen so far also 
 
-            # If number of distinct characters is greater than k
-            # We have seen more than k different characters 
-            # If our distinct character counter exceeds k, then we move the left pointer forward until we are within our bound
-            while num_distinct_chars > k:
+            # We have now seen more than k different characters - start iterating through the string with our left pointer 
+            # Remove the left most character from our substring 
+            # We will be dropping one character from our substring so we can look at others 
+            while num_distinct_chars > k: 
+                # We are now creating a substring within the given string with our left pointer 
                 # Decrement the counter for the char our left pointer is referencing 
+                # Updating left bound 
+                # Removing all instances of the left most character from the iterative substring and the hash map
+                # There is no point in calculating more distinct numbers than k
                 map[string[left]] -= 1
                 # This char isn't relevant to our final solution. 
                 # There is not enough instances of this character to be valid for our final answer. 
                 # We need at least k repeating characters. 
-                if map[string[left]] == 0:  # Remove this number from our map. We decremented it's count to equal 0 so we can remove it.
+                if map[string[left]] == 0:  # Remove this character from our map. We decremented it's count to equal 0 so we can remove it.
                     num_distinct_chars -= 1 # Number of distinct chars must change to reflect the char deletion from the map. 
                     del map[string[left]]
                 left += 1       # Advance the left pointer 
 
             # If the current window is greater than the max window, we update the max
+            # maximum is updated every iteration
+            # right pointer index - left pointer index + 1 since arrays and strings are 0 indexed  
             maximum = max(right - left + 1, maximum)
             right += 1 # Increment right pointer (main pointer)
 
@@ -84,6 +89,18 @@ class Solution:
 def main():
     print("hello world!")
     s = Solution()
+    ans = s.longestSubstringWithAtMostKDistinctCharacters("",2)
+    print(ans)
+    
+    ans = s.longestSubstringWithAtMostKDistinctCharacters("f",2)
+    print(ans)
+
+    ans = s.longestSubstringWithAtMostKDistinctCharacters("coccccoffee",2)
+    print(ans)
+
+    ans = s.longestSubstringWithAtMostKDistinctCharacters("cccccoffee",2)
+    print(ans)
+
     ans = s.longestSubstringWithAtMostKDistinctCharacters("coffee",2)
     print(ans)
 
@@ -108,7 +125,10 @@ def main():
     ans = s.longestSubstringWithAtMostKDistinctCharacters("ppf",2)
     print(ans)
 
-    ans = s.longestSubstringWithAtMostKDistinctCharacters("f",2)
+    ans = s.longestSubstringWithAtMostKDistinctCharacters("ffpzskfff",2)
+    print(ans)
+
+    ans = s.longestSubstringWithAtMostKDistinctCharacters("ccooff",2)
     print(ans)
 
     ans = s.longestSubstringWithAtMostKDistinctCharacters("f",2)
@@ -116,12 +136,6 @@ def main():
 
     ans = s.longestSubstringWithAtMostKDistinctCharacters("f",2)
     print(ans)
-
-    ans = s.longestSubstringWithAtMostKDistinctCharacters("f",2)
-    print(ans)
-
-
-
 
 if __name__ == "__main__":
     main()        
